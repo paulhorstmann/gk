0736394d791570c4bc49280e2429885fc3e4135a
Paul <paulluishorstmann@gmail.com>
2019-09-25 16:38:44 +0200

    Projekt Umstrukturierung

---
+++ src/taks/uebungen/zweivier/Kunde.java
@@ -0,0 +1,53 @@
+package info.herder.uebungen.zweivier;
+
+public class Kunde {
+    private String zName;
+    private String zAdresse;
+    private double zOffenerBetrag;
+
+    public Kunde(String pName, String pAdresse){
+        zName = pName;
+        zAdresse = pAdresse;
+    }
+
+    public void alleKundenRechnungenBegleichen(){
+        zOffenerBetrag = 0;
+    }
+
+    public void neueRechnungAnschreiben(double pBetrag){
+        zOffenerBetrag = zOffenerBetrag - pBetrag;
+    }
+
+    public void RechnungBegleichen(double pBetrag){
+        zOffenerBetrag = zOffenerBetrag + pBetrag;
+    }
+
+    public void setzeAdresse(String pAdresse){
+        zAdresse = pAdresse;
+    }
+
+    public void setzeName(String pName){
+        zName = pName;
+    }
+
+    public void setzeOffenerBetrag(double pBetrag){
+        zOffenerBetrag = pBetrag;
+    }
+
+    public String gibAdresse(){
+        return zAdresse;
+    }
+
+    public String gibName(){
+        return zName;
+    }
+
+    public String gibErinnerungsschreiben(){
+        System.out.println("Sehr geehrter Frau/Herr " + zName + ", sie haben noch einen Betrag von " + zOffenerBetrag + " Euro zu begleichen");
+        return "Sehr geehrter Frau/Herr " + zName + ", sie haben noch einen Betrag von " + zOffenerBetrag + " Euro zu begleichen";
+    }
+
+    public double gibOffenerBetrag(){
+        return zOffenerBetrag;
+    }
+}
---
+++ src/taks/uebungen/zweivier/Kundenverwaltung.java
@@ -0,0 +1,90 @@
+package info.herder.uebungen.zweivier;
+
+public class Kundenverwaltung {
+    private Kunde [] zKundenliste;
+    private int zKundenanzahl;
+
+    public Kundenverwaltung() {
+        zKundenliste = new Kunde [10000];
+        zKundenanzahl = 0;
+    }
+
+    public void KundendatenAusgeben(){
+        if(zKundenanzahl==0){
+            System.out.println("Keine Kunden vorhanden");
+            return;
+        }
+        System.out.println();
+        System.out.printf("%-7s %-20s %-25s %-30s %n", "Nummer", " Name", " Adresse", "Rechnungsbetrag");
+        Kunde lKunde = new Kunde("","");
+        for(int i = 0; i<zKundenanzahl; i++){
+            lKunde = zKundenliste[i];
+            System.out.format("%-8s %-20s %-25s %-29s %n",
+                    String.valueOf(i), lKunde.gibName(), lKunde.gibAdresse() , String.valueOf(lKunde.gibOffenerBetrag()));
+        }
+        System.out.println();
+    }
+
+    public boolean alleRechnungenBegleichen(String pName){
+        if(istKundeVorhanden(pName))return false;
+        zKundenliste[sucheKunden(pName)].alleKundenRechnungenBegleichen();
+        return true;
+    }
+
+    public String gibErinnerungsschreiben(String pName){
+        System.out.println();
+        if(istKundeVorhanden(pName))return "Error #404 | Kunde nicht vorhanden";
+        return zKundenliste[sucheKunden(pName)].gibErinnerungsschreiben();
+    }
+
+    public int gibKundenanzahl(){
+        return zKundenanzahl;
+    }
+
+    public boolean KundenLoeschen(String pName){
+        if(istKundeVorhanden(pName))return false;
+        for(int i = sucheKunden(pName); i<zKundenanzahl; i++)zKundenliste [i] = zKundenliste[i+1];
+        zKundenanzahl--;
+        System.out.println();
+        System.out.println(pName + " wurde gelöscht");
+        return true;
+    }
+
+    public boolean neuenKundenAnlegen(String pName, String pAdresse){
+        Kunde lKunde = new Kunde(pName, pAdresse);
+        zKundenliste [zKundenanzahl] = lKunde;
+        zKundenanzahl++;System.out.println();
+        System.out.println(pName + " wurde angelegt");
+        return true;
+    }
+
+    public boolean RechnungAnschreiben(String pName, double pBetrag){
+        if(istKundeVorhanden(pName))return false;
+        zKundenliste[sucheKunden(pName)].neueRechnungAnschreiben(pBetrag);
+        System.out.println();
+        System.out.println(pName + " wurde einen Betrag " + pBetrag + " abgebucht");
+        return true;
+    }
+
+    public boolean RechnungBegleichen(String pName, double pBetrag){
+        if(istKundeVorhanden(pName))return false;
+        zKundenliste[sucheKunden(pName)].RechnungBegleichen(pBetrag);
+        System.out.println();
+        System.out.println(pName + " wurde einen Betrag " + pBetrag + " gutgeschrieben");
+        return true;
+    }
+
+    public int sucheKunden(String pName){
+        for(int i = 0; i<zKundenanzahl; i++)
+            if(zKundenliste[i].gibName().equals(pName))
+                return i;
+        return -1;
+    }
+
+    private boolean istKundeVorhanden(String pName){
+        for(int i = 0; i<zKundenanzahl; i++)
+            if(zKundenliste[i].gibName().equals(pName))
+                return false;
+        return true;
+    }
+}
